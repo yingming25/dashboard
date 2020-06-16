a5aaa0377024eca5130940d5ff56158d2f50a6cf
yingming25 <karcnonf@gmail.com>
2020-06-16 17:12:42 +0800

    Update 2020-06-16T09:12:28.072Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,18 +1,20 @@
-$(document).ready(() => {
-  $('.replayQuestion').on('click', function(e){
+"use strict";
+
+$(document).ready(function () {
+  $('.replayQuestion').on('click', function (e) {
     e.preventDefault();
     $('.replyAnswer').removeClass('d-none');
     $('.replyAnswer').addClass('d-flex');
   });
-  $('.cancelReply').on('click', function(e){
+  $('.cancelReply').on('click', function (e) {
     e.preventDefault();
     $('.replyAnswer').removeClass('d-flex');
     $('.replyAnswer').addClass('d-none');
   });
-
-  $('.viewExpand').on('click', function(e){
+  $('.viewExpand').on('click', function (e) {
     e.preventDefault();
     $(this).toggleClass('active');
     $('.modal-footer').toggleClass('d-none');
-  })
+  });
 });
+//# sourceMappingURL=all.js.map
