2ed5b74dcdfc88c3653a3642976f3b36323c0db7
yingming25 <karcnonf@gmail.com>
2020-06-16 17:06:02 +0800

    製作 Admin 頁面

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
 $(document).ready(() => {
-  console.log('Hello Bootstrap4');
+  $('.replayQuestion').on('click', function(e){
+    e.preventDefault();
+    $('.replyAnswer').removeClass('d-none');
+    $('.replyAnswer').addClass('d-flex');
+  });
+  $('.cancelReply').on('click', function(e){
+    e.preventDefault();
+    $('.replyAnswer').removeClass('d-flex');
+    $('.replyAnswer').addClass('d-none');
+  });
+
+  $('.viewExpand').on('click', function(e){
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $('.modal-footer').toggleClass('d-none');
+  })
 });
